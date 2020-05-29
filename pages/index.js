74f724676d82195ad1fc4dea00f170380714724a
Alex Semenets <alex.semenec@code-care.pro>
2020-05-29 08:22:15 +0300

    wip: progress on 28.05

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,169 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: Nunito;
+}
+`;
+
+const Header = styled.h2`
+  font-size: 36px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: ${(props) => props.color || 'inherit'};
+`;
+
+const GradientHeader = styled(Header)`
+  background-image: linear-gradient(
+    ${(props) => props.angle + 'deg'},
+    ${(props) => props.gradientFrom},
+    ${(props) => props.gradientTo}
+  );
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  display: inline-block;
+`;
+
+const Button = styled.a`
+  padding: 14px 25px;
+  color: #fff;
+  background-color: #ff7702;
+  border-radius: 5px;
+  font-family: Nunito;
+  font-weight: bold;
+  transition: 0.25s;
+  cursor: pointer;
+  &:hover {
+    background-color: #e66b00;
+  }
+`;
+
+const NavBar = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavItem = styled.a`
+  font-size: 16px;
+  font-weight: bold;
+  font-family: Nunito;
+  transition: 0.25s;
+  display: inline-block;
+  margin-right: 40px;
+  cursor: pointer;
+  &:hover {
+    color: #1f7be8;
+  }
+`;
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
+      <GlobalStyle />
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>ProRaise</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Nunito&display=swap'
+          rel='stylesheet'
+        />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <NavBar>
+        <Header as='h1' color='#ff7702' style={{ fontWeight: '300' }}>
+          ProRaise
+        </Header>
+        <div>
+          <NavItem>Benefits</NavItem>
+          <NavItem>Features</NavItem>
+          <NavItem>Login</NavItem>
+          <Button>Sign up</Button>
         </div>
+      </NavBar>
+
+      <main>
+        <section>
+          <Header>
+            Help founders navigate the capital raising process simply
+          </Header>
+          <p>
+            Scaling your business may need funding from investors. This can be a
+            time consuming and complex process.
+          </p>
+          <p>
+            ProRaise helps you run your fundraising more efficiently, saving you
+            time.
+          </p>
+          <Button>Start fundraising</Button>
+        </section>
+
+        <section>
+          <GradientHeader
+            gradientFrom='#00f091'
+            gradientTo='#009efb'
+            angle='106'
+          >
+            Benefits
+          </GradientHeader>
+          <p>Navigate the complex capital raising process simply</p>
+          <p>
+            Present your company professionally to investors to improve your
+            chance of closing quickly
+          </p>
+          <p>Accelerate your investor discussions</p>
+        </section>
+
+        <section>
+          <GradientHeader
+            gradientFrom='#ff9b49'
+            gradientTo='#ffc92b'
+            angle='285'
+          >
+            Features
+          </GradientHeader>
+          <div>
+            <h3>Create your pitch</h3>
+            <p>Present your company professionally</p>
+          </div>
+          <div>
+            <h3>Set up your deal</h3>
+            <p>Clear and professional, done within minutes</p>
+          </div>
+          <div>
+            <h3>Share with your network</h3>
+            <p>Fast and simple way to engage with investors</p>
+          </div>
+          <div>
+            <h3>Secure dataroom</h3>
+            <p>Easy to set up and manage permissions</p>
+          </div>
+          <div>
+            <Header as='h3'>Gain feedback & insight</Header>
+            <p>Receive helpful feedback and insights</p>
+          </div>
+          <div>
+            <h3>Track interests</h3>
+            <p>Instant status update</p>
+          </div>
+        </section>
+
+        <section>
+          <h3>Get started on a 1 month free trial!</h3>
+          <p>It is only ￡50/month* with no fixed contract.</p>
+          <p>You can cancel anytime if it doesn't help you close your round!</p>
+          <Button>Start your 1-month free trial</Button>
+          <p>*excl. VAT</p>
+        </section>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <footer></footer>
     </div>
-  )
+  );
 }
